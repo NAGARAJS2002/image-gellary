77afb71e846e17ae02b3b7f9c20ebc9d640d18af
NAGARAJS2002 <nr2277644@gmail.com>
2024-12-23 18:22:09 +0530

    create api routes and test

--- backend/index.js
+++ backend/index.js
@@ -1,12 +1,13 @@
 import express from "express";
+import userRouter from "./routes/userRoute.js";
 const app = express();
 const PORT = 3000;
 
 
-
-
+app.use('/api/user',userRouter);
 
 app.listen(PORT, () => {
     console.log(`server running on port ${PORT}`);
     
-})
+});
+
