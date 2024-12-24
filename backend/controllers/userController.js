ca8ca3c00e38724356617516b34496cba8e15af1
NAGARAJS2002 <nr2277644@gmail.com>
2024-12-24 15:55:46 +0530

    complete login api route

--- backend/controllers/userController.js
+++ backend/controllers/userController.js
@@ -1,6 +1,6 @@
 import bcryptjs from "bcryptjs"
 import User from "../models/userModel.js"
-
+import jwt from "jsonwebtoken"
 export const register = async (req,res,next) => {
    const {username,email,password} = req.body;
    if (!username||!email||!password){
@@ -25,3 +25,23 @@ export const register = async (req,res,next) => {
        next(error);
    }
 }
+
+
+export const login = async (req,res,next)=>{
+   const {email,password} = req.body;
+   try {
+            const validUser = await User.findOne({email})
+            if (!validUser) return next(errorHandler(404,`user not found `));
+     
+            const validPassword = bcryptjs.compareSync(password,validUser.password);
+            if(!validPassword) return next(errorHandler(401,'wrong credential!'));
+     
+            const token = jwt.sign({id: validUser._id}, process.env.JWT_SECRET)
+            res.cookie('access_token',token,{http:true}).
+            status(202).
+            json(validUser);
+     
+       } catch (error) {
+       next(error )
+   }
+};
