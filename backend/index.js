1cbc65b42ab4119aab778b444ac2b151dd1398cf
NAGARAJS2002 <nr2277644@gmail.com>
2024-12-24 15:42:32 +0530

    complete register api route

--- backend/index.js
+++ backend/index.js
@@ -1,15 +1,17 @@
 import express from "express";
 import mongoose from "mongoose";
+import bodyParser from "body-parser";
 import userRouter from "./routes/userRoute.js";
 import env from "dotenv"
 env.config();
 const app = express();
 const PORT = 3000;
 
+app.use(bodyParser.json())
 mongoose.connect(process.env.MONGODB_URL).then(()=>{
   console.log('database connected');
   
-})
+}).catch((error) => console.error('Error connecting to MongoDB:', error));
 
 
 app.use('/api/user',userRouter);
