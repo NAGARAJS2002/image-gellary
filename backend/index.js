dd8cac4c791e8d3d64bd4633fd4a27b3c70923ad
NAGARAJS2002 <nr2277644@gmail.com>
2024-12-24 15:03:32 +0530

    connected mongodb database

--- backend/index.js
+++ backend/index.js
@@ -1,7 +1,15 @@
 import express from "express";
+import mongoose from "mongoose";
 import userRouter from "./routes/userRoute.js";
+import env from "dotenv"
+env.config();
 const app = express();
 const PORT = 3000;
+
+mongoose.connect(process.env.MONGODB_URL).then(()=>{
+  console.log('database connected');
+  
+})
 
 
 app.use('/api/user',userRouter);
@@ -11,3 +19,13 @@ app.listen(PORT, () => {
     
 });
 
+app.use((err,req,res,next) => {
+    const statusCode = err.statusCode || 500;
+    const message = err.message || 'Internal Server Error'
+
+    return res.status(statusCode).json({
+      success: false,
+      statusCode,  
+      message,
+    })
+})
